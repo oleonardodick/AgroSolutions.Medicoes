0742d184a0971c672e895f31579b89a5a17e0154
Leonardo Bernardes <leonardod.bernardes@gmail.com>
2026-01-24 16:17:05 -0300

    feat: inicio stack kubernetes

---
+++ scripts/create-secrets.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+ENV_FILE="$ROOT_DIR/.env"
+NAMESPACE="agro-medicoes"
+RABBIT_SECRET="rabbitmq-auth"
+GRAFANA_SECRET="grafana-auth"
+DATABASE_SECRET="database-config"
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo "❌ Arquivo .env não encontrado em $ENV_FILE"
+    exit 1
+fi
+
+set -a
+source "$ENV_FILE"
+set +a
+
+# validações
+: "${RABBITMQ_DEFAULT_USER:?Variável RABBITMQ_DEFAULT_USER não definida}"
+: "${RABBITMQ_DEFAULT_PASS:?Variável RABBITMQ_DEFAULT_PASS não definida}"
+
+: "${GF_SECURITY_ADMIN_USER:?Variável GF_SECURITY_ADMIN_USER não definida}"
+: "${GF_SECURITY_ADMIN_PASSWORD:?Variável GF_SECURITY_ADMIN_PASSWORD não definida}"
+
+: "${DB_HOST:?Variável DB_HOST não definida}"
+: "${DB_PORT:?Variável DB_PORT não definida}"
+: "${DB_USER:?Variável DB_USER não definida}"
+: "${DB_PASSWORD:?Variável DB_PASSWORD não definida}"
+: "${DB_NAME:?Variável DB_NAME não definida}"
+
+echo "🔐 Criando secret: $RABBIT_SECRET"
+kubectl create secret generic $RABBIT_SECRET \
+    -n $NAMESPACE \
+    --from-literal=RABBITMQ_DEFAULT_USER="$RABBITMQ_DEFAULT_USER" \
+    --from-literal=RABBITMQ_DEFAULT_PASS="$RABBITMQ_DEFAULT_PASS" \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+echo "🔐 Criando secret: $GRAFANA_SECRET"
+kubectl create secret generic $GRAFANA_SECRET \
+    -n $NAMESPACE \
+    --from-literal=GF_SECURITY_ADMIN_USER="$GF_SECURITY_ADMIN_USER" \
+    --from-literal=GF_SECURITY_ADMIN_PASSWORD="$GF_SECURITY_ADMIN_PASSWORD" \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+echo "🔐 Criando secret: $DATABASE_SECRET"
+kubectl create secret generic $DATABASE_SECRET \
+    -n $NAMESPACE \
+    --from-literal=DB_HOST="$DB_HOST" \
+    --from-literal=DB_PORT="$DB_PORT" \
+    --from-literal=DB_USER="$DB_USER" \
+    --from-literal=DB_PASSWORD="$DB_PASSWORD" \
+    --from-literal=DB_NAME="$DB_NAME" \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+echo "✅ Secrets aplicados com sucesso!"
---
+++ scripts/dev-deploy.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ========= CONFIG =========
+CLUSTER_NAME="dev-worker"
+KIND_CONFIG_FILE="k8s/kind/config.yaml"
+
+# ========= PATHS =========
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+# ========= HELPERS =========
+log() {
+  echo "▶ $1"
+}
+
+error() {
+  echo "❌ $1" >&2
+  exit 1
+}
+
+check_command() {
+  command -v "$1" >/dev/null 2>&1 || error "Comando '$1' não encontrado"
+}
+
+# ========= CHECKS =========
+log "Verificando dependências"
+check_command kubectl
+check_command kind
+
+# ========= CLUSTER =========
+if kind get clusters | grep -q "^${CLUSTER_NAME}$"; then
+    log "Cluster Kind '$CLUSTER_NAME' já existe"
+else
+    log "Criando cluster Kind '$CLUSTER_NAME'"
+    if [[ -f "$ROOT_DIR/$KIND_CONFIG_FILE" ]]; then
+        kind create cluster \
+        --name "$CLUSTER_NAME" \
+        --config "$ROOT_DIR/$KIND_CONFIG_FILE"
+    else
+        log "Arquivo de configuração não existe"
+        exit 1
+    fi
+fi
+
+
+# ========= WAIT CLUSTER =========
+log "Aguardando cluster ficar acessível"
+kubectl wait --for=condition=Ready nodes --all --timeout=120s
+
+CURRENT_CONTEXT="$(kubectl config current-context)"
+if [[ "$CURRENT_CONTEXT" != kind-* ]]; then
+  error "Contexto atual ($CURRENT_CONTEXT) não é um cluster Kind"
+fi
+
+# ========= DEPLOY =========
+log "Criando namespaces"
+kubectl apply -f "$ROOT_DIR/k8s/base/namespaces"
+
+log "Criando secrets"
+bash "$ROOT_DIR/scripts/create-secrets.sh"
+
+log "Aplicando manifests base"
+kubectl apply -f "$ROOT_DIR/k8s/base/mailpit"
+kubectl apply -f "$ROOT_DIR/k8s/base/rabbitmq"
+kubectl create configmap grafana-datasources \
+    --from-file=$ROOT_DIR/observability/grafana/provisioning/datasources.yaml \
+    -n agro-medicoes \
+    --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -f "$ROOT_DIR/k8s/base/grafana"
+
+log "Deploy concluído com sucesso ✅"
